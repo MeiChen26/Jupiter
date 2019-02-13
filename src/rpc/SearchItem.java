c77a8794fcf0a82e6b0f510459a4fb4b8046edf0
meitong <meitong@meitongdeMacBook-Pro.local>
2019-02-13 14:16:53 +0800

    update css

--- src/rpc/SearchItem.java
+++ src/rpc/SearchItem.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -19,6 +20,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class SearchItem
@@ -39,6 +41,15 @@ public class SearchItem extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// allow access only if session exists
+//				HttpSession session = request.getSession(false);
+//				if (session == null) {
+//					response.setStatus(403);
+//					return;
+//				}
+
+
+		String userId = request.getParameter("user_id");
 		double lat = Double.parseDouble(request.getParameter("lat"));
 		double lon = Double.parseDouble(request.getParameter("lon"));
 		// Term can be empty or null.
@@ -46,14 +57,20 @@ public class SearchItem extends HttpServlet {
 
 		DBConnection connection = DBConnectionFactory.getConnection();
 		List<Item> items = connection.searchItems(lat, lon, term);
- 		connection.close();
+		Set<String> favorite = connection.getFavoriteItemIds(userId);
+		connection.close();
+
 
 		List<JSONObject> list = new ArrayList<>();
 		try {
 			for (Item item : items) {
 				// Add a thin version of item object
 				JSONObject obj = item.toJSONObject();
-				list.add(obj);
+				
+				// Check if this is a favorite one.
+				// This field is required by frontend to correctly display favorite items.
+				obj.put("favorite", favorite.contains(item.getItemId()));
+                list.add(obj);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
